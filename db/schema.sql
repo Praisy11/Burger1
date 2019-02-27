73bb6446b359647b8257ea2d8bb4e949df6d8845
Praisy11 <praisy_john@yahoo.com>
2019-02-27 13:22:55 -0500

    I

--- db/schema.sql
+++ db/schema.sql
@@ -4,8 +4,9 @@ USE burgers_db;
 -- Create the table plans.
 CREATE TABLE burgers
 (
-id int NOT NULL AUTO_INCREMENT,
+id int  AUTO_INCREMENT NOT NULL ,
 burger_name varchar(255) NOT NULL,
+createdAt timestamp NOT NULL,
 devoured boolean DEFAULT false,
 PRIMARY KEY (id)
 );
